c251fcc615c8dfcec94d35d0ae97e0275eeeb50b
VK <VK@VKVKVK.de>
2016-09-24 23:10:05 +0200

    added comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,36 +1,43 @@
 library(plyr)
 
+#
+# Global variables: paths, data source
+#
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 dataDir <- "data"
 dataZipFile <- file.path(dataDir, "ucidata.zip")
 workingDir <- file.path(dataDir, "UCI HAR Dataset")
 outDir <- "output"
 
-
+#
+# download file function
+#
 downloadFile <- function() {
+    # test for the rawdata file
    if (!file.exists(dataZipFile)) {
-
         if (!file.exists(dataDir)) {
             dir.create(dataDir)
         }
-
         download.file(fileUrl, dest=dataZipFile, method="curl")
     }
     unzip(dataZipFile, exdir=dataDir)
 }
 
+#
+# test for working directory, start download if needed
+#
 getData <- function() {
 
     if (!file.exists(workingDir)) {
         downloadFile()
     }
     setwd(workingDir)
- 
-    
 }
 
 
-
+#
+# read data, add activity and subject cols
+#
 readData <- function(dname, colnames) {
     d <- read.table(file.path(dname,sprintf("X_%s.txt",dname)), col.names=colnames)
     d[,"activity"] <- read.table(file.path(dname,sprintf("y_%s.txt",dname)))
@@ -38,7 +45,11 @@ readData <- function(dname, colnames) {
     d
 }
 
+#
+# merge train and test data sets
+#
 mergeSets <- function() {
+    # set descriptive column names
     dataColnames <- as.character(read.table("features.txt")[,2])
     trainData <- readData("train", dataColnames)
     testData <- readData("test", dataColnames)
@@ -47,6 +58,9 @@ mergeSets <- function() {
 }
 
 
+#
+# analyze data
+#
 analyzeData <- function(dataset) {
     
     # grep columns with mean, std, keep activity and subject columns
@@ -63,6 +77,9 @@ analyzeData <- function(dataset) {
     meanAndStdAv
 }
 
+#
+# write output file
+#
 writeOut <- function(tidydata, outDir) {
     setwd("../..")
     if (!file.exists(outDir)) { 
@@ -71,7 +88,9 @@ writeOut <- function(tidydata, outDir) {
     write.table(tidydata, file.path(outDir,"tidy.txt"), row.name=FALSE)
 }
 
-
+#
+# "main" steps
+#
 getData()
 m <- mergeSets()
 tidydata <- analyzeData(m)
